9b7a23a76fd3a64327aa3bbdfa872ff9daf03556
James <jamesmca@live.com>
2021-06-04 13:51:21 -0700

    updated images
    text curve is finally working......

--- src/components/TiakiIka.js
+++ src/components/TiakiIka.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import * as shirtStyles from './taikaiwi.module.css'
-import shark from '../assets/images/sharktransparent.png'
+import shark from '../assets/images/goku.png'
 
 const TiakiIka = () => {
-    const radius = 300
-    const arc = 180
+    const radius = 400
+    const arc = 160
     const characters = ["T", "I", "A", "K", "I"]
-    const degree = ['15', '60', '90', '125', '160']
+    const jap = ["善", '意', 'の', '味', '方']
+    const degree = arc / jap.length
     console.log(radius, arc, characters, degree)
 
     const styles = ['-300px', '-108px', '-22px', '-45px', '-210px']
@@ -16,35 +17,39 @@ const TiakiIka = () => {
 
     return (
         <div className={shirtStyles.taikaiwiContainer}>
-            {/* <div style={{ display: 'flex', flexDirection: 'row', marginBottom: '110px', position: 'relative', border: '2px solid white' }}>
-                {characters.map((char, i) => (
-                    <span style={{ display: 'inline-block', bottom: styles[i], left: left[i], transform: `rotate(${rotations[i]})` }}>
-                        <h1 style={{transform: char === "K" ? 'scale(0.8, 0.7)' : 'scale(1, 0.7)'}}>{char}</h1>
-                    </span>
-                ))}
-            </div> */}
-
-            {/* <h1 className={shirtStyles.moveable}>
-                {characters.map((char, i) => (
+            {/* <h2>善意の味方</h2> */}
+            <h1 className={shirtStyles.moveable}>
+                {jap.map((char, i) => (
                     <span
                         key={`heading-span-${i}`}
                         style={{
                             height: `${radius}px`,
-                            transform: `rotate(${degree[i] - arc / 2 - 10}deg)`,
+                            transform: `rotate(${degree * i - arc / 2 + 7}deg)`,
+                            transformOrigin: `0 ${radius}px 0`,
                         }}>
                         {char}
                     </span>
                 ))}
-            </h1> */}
-            {/* <h1>TIAKI</h1> */}
-            {/* <h1 className={shirtStyles.depth} title="TAIKA">TAIKA</h1> */}
-            <h2>TIAKI</h2>
+            </h1>
+
             <img src={shark} alt="shark" />
-            <h2>IKA</h2>
-            <h2>海を救う</h2>
+            {/* <h2>IKA</h2> */}
             {/* <h1 className={shirtStyles.depth} title="IWI">IWI</h1> */}
         </div>
     )
 }
 
 export default TiakiIka
+
+//  {/* <div style={{ display: 'flex', flexDirection: 'row', marginBottom: '110px', position: 'relative', border: '2px solid white' }}>
+//                 {characters.map((char, i) => (
+//                     <span style={{ display: 'inline-block', bottom: styles[i], left: left[i], transform: `rotate(${rotations[i]})` }}>
+//                         <h1 style={{transform: char === "K" ? 'scale(0.8, 0.7)' : 'scale(1, 0.7)'}}>{char}</h1>
+//                     </span>
+//                 ))}
+//             </div> */}
+
+            
+            // {/* <h1>TIAKI</h1> */}
+            // {/* <h1 className={shirtStyles.depth} title="TAIKA">TAIKA</h1> */}
+            /* <h2>TIAKI</h2> */
