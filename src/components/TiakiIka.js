3aff7840a490acbbda2c593f76ca08bfbf79621f
James <jamesmca@live.com>
2021-06-04 08:50:50 -0700

    more color changes

--- src/components/TiakiIka.js
+++ src/components/TiakiIka.js
@@ -3,35 +3,44 @@ import * as shirtStyles from './taikaiwi.module.css'
 import shark from '../assets/images/sharktransparent.png'
 
 const TiakiIka = () => {
-    const radius = 150
-    const arc = 1000
+    const radius = 300
+    const arc = 180
     const characters = ["T", "I", "A", "K", "I"]
-    const degree = arc / characters.length
+    const degree = ['15', '60', '90', '125', '160']
     console.log(radius, arc, characters, degree)
 
-    const styles = []
+    const styles = ['-300px', '-108px', '-22px', '-45px', '-210px']
+    const left = ['-305px', '-220px', '-89px', '135px', '270px']
+    const rotations = ['-75deg', '-50deg', '-1deg', '46deg', '75deg']
+
 
     return (
         <div className={shirtStyles.taikaiwiContainer}>
-            <div style={{display: 'flex', flexDirection: 'row'}}>
-                {characters.map(char => (
-                    <h1 style={{marginBottom: '-40px'}}>{char}</h1>
+            {/* <div style={{ display: 'flex', flexDirection: 'row', marginBottom: '110px', position: 'relative', border: '2px solid white' }}>
+                {characters.map((char, i) => (
+                    <span style={{ display: 'inline-block', bottom: styles[i], left: left[i], transform: `rotate(${rotations[i]})` }}>
+                        <h1 style={{transform: char === "K" ? 'scale(0.8, 0.7)' : 'scale(1, 0.7)'}}>{char}</h1>
+                    </span>
                 ))}
-            </div>
+            </div> */}
 
             {/* <h1 className={shirtStyles.moveable}>
                 {characters.map((char, i) => (
                     <span
                         key={`heading-span-${i}`}
-                        className={shirtStyles.char}>
+                        style={{
+                            height: `${radius}px`,
+                            transform: `rotate(${degree[i] - arc / 2 - 10}deg)`,
+                        }}>
                         {char}
                     </span>
                 ))}
             </h1> */}
             {/* <h1>TIAKI</h1> */}
             {/* <h1 className={shirtStyles.depth} title="TAIKA">TAIKA</h1> */}
+            <h2>TIAKI</h2>
             <img src={shark} alt="shark" />
-            <h1 >IKA</h1>
+            <h2>IKA</h2>
             {/* <h1 className={shirtStyles.depth} title="IWI">IWI</h1> */}
         </div>
     )
