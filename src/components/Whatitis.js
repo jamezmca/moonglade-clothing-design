4f066715314b4daa77456e88f2c6bf4327d7639f
James <jamesmca@live.com>
2021-06-06 21:50:19 -0700

    dinosauurrssss diplodicus

--- src/components/Whatitis.js
+++ src/components/Whatitis.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import * as shirtStyles from './whatitis.module.css'
+import shark from '../assets/images/goku.png'
+
 
 const Whatitis = () => {
-    const radius = 800
-    const arc = 200
+    const radius = 2000
+    const arc = 30
+    const text = 'galacticool'
     const characters = ["T", "I", "A", "K", "I"]
-    const jap = [' ', "I","T", " ", "I", "S", ' ', 'W', 'H', 'A', 'T', ' ', 'I', 'T', ' ', 'I', 'S']
+    // const jap = [' ', "I","T", " ", "I", "S", ' ', 'W', 'H', 'A', 'T', ' ', 'I', 'T', ' ', 'I', 'S']
+    // const jap = ["C", 'U', 'R', 'J', 'O', 'S', 'J', 'T', 'Y']
+    const jap = text.split('')
     const degree = arc / jap.length
     
     return (
@@ -16,13 +21,15 @@ const Whatitis = () => {
                         key={`heading-span-${i}`}
                         style={{
                             height: `${radius}px`,
-                            transform: `rotate(${degree * i - arc / 2 }deg)`,
+                            transform: char == 'i' ? `rotate(${degree * i - arc / 2 }deg)` : `rotate(${degree * i - arc / 2 }deg)`,
                             transformOrigin: `0 ${radius}px 0`,
                         }}>
                         {char}
                     </span>
                 ))}
             </h1>
+            <img src={shark} alt="shark" />
+
         </div>
     )
 }
